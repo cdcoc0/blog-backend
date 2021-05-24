7c87db97056b84d77ab56c2f9cd6996c2846f464
velykitty <jiyunee13@gmail.com>
2021-05-24 18:14:35 +0900

    apply import/export grammar of ES module using esm -add esm -convert codes to ES Module -set word completion in jsonconfig

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -13,7 +13,7 @@ const posts = [
 POST /api/posts
 {title, body}
 */
-exports.write = ctx => {
+export const write = ctx => {
     //REST API의 Request Body는 ctx.request.body에서 조회 가능
     const {title, body} = ctx.request.body;
     postId += 1; //기존 id값에 1 더함
@@ -25,14 +25,14 @@ exports.write = ctx => {
 /* 포스트 목록 조회
 GET api/posts
 */
-exports.list = ctx => {
+export const list = ctx => {
     ctx.body = posts;
 };
 
 /* 특정 포스트 조회
 GET api/posts/:id
 */
-exports.read = ctx => {
+export const read = ctx => {
     const {id} = ctx.params;
     //주어진 id값으로 포스트를 찾음
     //파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
@@ -53,7 +53,7 @@ exports.read = ctx => {
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-exports.remove = ctx => {
+export const remove = ctx => {
     const {id} = ctx.params;
 
     //해당 id를 가진 post가 몇 번째인지 확인
@@ -76,7 +76,7 @@ exports.remove = ctx => {
 PUT api/posts/:id
 {title, body}
 */
-exports.replace = ctx => {
+export const replace = ctx => {
     //PUT 메서드는 전체 포스트 정보를 입력해 데이터를 통째로 교체할 떄 사용
     const {id} = ctx.params;
 
@@ -105,7 +105,7 @@ exports.replace = ctx => {
 PATCH /api/posts/:id
 {title, body}
 */
-exports.update = ctx => {
+export const update = ctx => {
     //PATCH 메서드는 주어진 필드만 교체
     const {id} = ctx.params;
     
