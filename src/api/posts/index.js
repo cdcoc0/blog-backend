7ed4c0090e3f4f1426133073af1441bf8e598ee0
velykitty <jiyunee13@gmail.com>
2021-06-29 21:12:03 +0900

    check whether the user is logged in

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,18 +1,23 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
-//const post = new Router(); // /api/posts/:id
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
-//posts.put('/:id', postsCtrl.replace);
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.update)
 
-//posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+// posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+// posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+// posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+// posts.put('/:id', postsCtrl.replace);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 
 export default posts;
